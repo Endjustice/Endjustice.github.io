017aab994d9e9dedf39c68eeb82d4037d0ad4d9b
Endjustice <m.ohammad.xxx13@gmail.com>
2025-12-29 02:11:08 +0330

    Final fix: Java ID typo corrected and system icon restored

--- app/src/main/java/com/example/webwrapperapp/MainActivity.java
+++ app/src/main/java/com/example/webwrapperapp/MainActivity.java
@@ -1,51 +1,21 @@
 package com.example.webwrapperapp;
 
 import android.os.Bundle;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     private WebView webView;
-    private static final String TARGET_URL = "https://myai.kronos666.workers.dev/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        webView = findViewById(R.id.webview);
-
-        // تنظیمات WebView
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setDomStorageEnabled(true);
-        webSettings.setLoadWithOverviewMode(true);
-        webSettings.setUseWideViewPort(true);
-
-        // جلوگیری از باز شدن لینک‌ها در مرورگر خارجی
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                view.loadUrl(request.getUrl().toString());
-                return true;
-            }
-        });
-
-        // بارگذاری آدرس هدف
-        webView.loadUrl(TARGET_URL);
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.google.com");
     }
 }
